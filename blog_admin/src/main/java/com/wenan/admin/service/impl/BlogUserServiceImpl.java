af80fe7797ea73e337b81b4bc68bf39b976ad955
Wenan <799210382@qq.com>
2021-12-27 17:03:13 +0800

    1.8 修改，用户列表只展示isvalid为0的用户

--- blog_admin/src/main/java/com/wenan/admin/service/impl/BlogUserServiceImpl.java
+++ blog_admin/src/main/java/com/wenan/admin/service/impl/BlogUserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.wenan.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wenan.admin.entity.BlogUser;
@@ -25,8 +26,10 @@ public class BlogUserServiceImpl extends ServiceImpl<BlogUserMapper, BlogUser> i
     public Page<BlogUser>  listUser(Integer page, Integer limit) {
         // 1 获取分页对象
         Page<BlogUser> blogUserPage = new Page<>(page, limit);
-        // 2 传入user对象查找
-        baseMapper.selectPage(blogUserPage, null);
+        // 2 传入user对象查找,查找isvalid为0的用户信息
+        QueryWrapper<BlogUser> blogUserQueryWrapper = new QueryWrapper<>();
+        blogUserQueryWrapper.eq("isvalid", 0);
+        baseMapper.selectPage(blogUserPage, blogUserQueryWrapper);
         return blogUserPage;
     }
 }
