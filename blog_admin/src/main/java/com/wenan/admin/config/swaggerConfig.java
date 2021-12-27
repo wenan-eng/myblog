eb76da8d68b2e462dac9e1a4f5e06e913cc7ef21
Wenan <799210382@qq.com>
2021-12-27 16:09:55 +0800

    1.2 配置swagger2文件，并启动swagger2

---
+++ blog_admin/src/main/java/com/wenan/admin/config/swaggerConfig.java
@@ -0,0 +1,40 @@
+package com.wenan.admin.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 描述：    swaggerConfig
+ */
+@Configuration
+@EnableSwagger2
+public class swaggerConfig {
+    @Bean
+    public Docket webApiConfig(){
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("webApi")
+                .apiInfo(webApiInfo())
+                .select()
+                .build();
+
+    }
+
+    private ApiInfo webApiInfo(){
+
+        return new ApiInfoBuilder()
+                .title("博客API文档")
+                .description("本文档描述了博客的各类api接口")
+                .version("1.0")
+                .contact(new Contact("wenan", "xxxx.com", "799210382@qq.com"))
+                .build();
+    }
+}
