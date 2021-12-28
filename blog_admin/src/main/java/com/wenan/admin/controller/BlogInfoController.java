88cc2b839ab7552dc2bbf021fdcafc6e663fa6ee
Wenan <799210382@qq.com>
2021-12-28 11:34:27 +0800

    2.3 新增，新增博客方法

--- blog_admin/src/main/java/com/wenan/admin/controller/BlogInfoController.java
+++ blog_admin/src/main/java/com/wenan/admin/controller/BlogInfoController.java
@@ -1,21 +1,59 @@
 package com.wenan.admin.controller;
 
 
+import com.wenan.admin.common.R;
+import com.wenan.admin.entity.BlogInfo;
+import com.wenan.admin.exception.BlogException;
+import com.wenan.admin.service.BlogInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author wenan
  * @since 2021-12-27
  */
+@Api(description = "博客信息")
 @RestController
-@RequestMapping("/admin/blog-info")
+@RequestMapping("/admin/info")
 public class BlogInfoController {
+    @Resource
+    private BlogInfoService infoService;
 
+    /**
+     * 新增博客
+     */
+    @ApiOperation(value = "新增博客")
+    @PostMapping("addBlog")
+    public R addBlog(@RequestBody BlogInfo blogInfo) {
+        boolean save = infoService.save(blogInfo);
+        if (!save) {
+            throw new BlogException(20002, "保存失败");
+        }
+        return R.ok();
+    }
+
+
+    /**
+     *  删除博客
+     */
+
+    /**
+     *  修改博客
+     */
+
+    /**
+     *  分页展示博客
+     */
 }
 
